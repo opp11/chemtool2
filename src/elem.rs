a18c5922663ef8f3adf870a171545e3a9d003e71
Patrick M Jensen <patmjen@gmail.com>
2015-01-29 17:29:16 +0100

    Add function to group duplicate elements
    For molecules such as CH3CH2CH2CH3 there is no reason to
    look up the for C and H multiple times. Therefore this
    function will group the elements so they only appear once
    in the molecule's formula. E.g. CH3CH2CH3 becomes C3H8.

--- src/elem.rs
+++ src/elem.rs
@@ -7,3 +7,36 @@ pub struct PerElem {
 }
 
 pub type Molecule = Vec<PerElem>;
+
+pub fn group_elems(molecule: Molecule) -> Molecule {
+    let mut out = Vec::<PerElem>::new();
+    for elem in molecule.into_iter() {
+        if let Some(pos) = out.iter().position(|e| e.name == elem.name) {
+            out[pos].coef += elem.coef;
+        } else {
+            out.push(elem);
+        }
+    }
+    out
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! dummy_elem(
+        ($name:expr) => (
+            PerElem { name: $name.to_string(), coef: 1, pos: 0, len: 1 }
+        );
+        ($name:expr, $coef:expr) => (
+            PerElem { name: $name.to_string(), coef: $coef, pos: 0, len: 1 }
+        );
+    );
+
+    #[test]
+    fn group() {
+        let result = group_elems(vec!(dummy_elem!("C"), dummy_elem!("H"), dummy_elem!("C")));
+        let expected = vec!(dummy_elem!("C", 2), dummy_elem!("H", 1));
+        assert_eq!(result, expected);
+    }
+}

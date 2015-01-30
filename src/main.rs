e4659b14c4e0f9184932d4f24cae7055cf18b5a2
Patrick M Jensen <patmjen@gmail.com>
2015-01-30 21:14:01 +0100

    Use docopt to parse cli arguments
    The program will now get the molecule to parse from the
    given commanline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(collections, path, io, core, os)]
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate docopt;
 
 use std::os;
+use docopt::Docopt;
 
 mod elem;
 mod parser;
@@ -8,12 +11,27 @@ mod error;
 mod database;
 mod mass;
 
+const USAGE: &'static str = "
+usage: chemtool <formula>
+       chemtool [-h | --help]
+
+options:
+    -h --help    Display this message and then exit.
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_formula: String,
+}
+
 #[cfg(not(test))]
 fn main() {
-    let input = "CH3";
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
     let mut path = Path::new(&os::args()[0]);
     path.set_filename("elemdb.csv");
-    if let Err(e) = mass::pretty_print_mass(input, &path) {
+    if let Err(e) = mass::pretty_print_mass(args.arg_formula.as_slice(), &path) {
         println!("{:?}", e);
     }
 }

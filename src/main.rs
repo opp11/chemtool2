076b6d9a9ad96855008e85e53a9841a87bb86cfd
Patrick M Jensen <patmjen@gmail.com>
2015-01-31 00:14:13 +0100

    Add version support and bump version to 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,14 @@ const USAGE: &'static str = "
 usage:
     chemtool <formula>
     chemtool [-h | --help]
+    chemtool [-v | --version]
 
 options:
-    -h --help    Display this message and then exit.
+    -h --help     Display this message and then exit.
+    -v --version  Display the version number and then exit.
 ";
+
+const VERSION: &'static str = "0.1.0";
 
 #[derive(RustcDecodable)]
 struct Args {
@@ -29,7 +33,11 @@ struct Args {
 #[cfg(not(test))]
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
+                            .and_then(|d| {
+                                d.help(true)
+                                 .version(Some(VERSION.to_string()))
+                                 .decode()
+                            })
                             .unwrap_or_else(|e| e.exit());
     let mut path = Path::new(&os::args()[0]);
     path.set_filename("elemdb.csv");

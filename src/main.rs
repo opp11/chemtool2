903b1f18af094b58211b8d2d2e4a4e097626f916
Patrick M Jensen <patmjen@gmail.com>
2015-02-02 18:02:23 +0100

    Switch to using docopt_macros

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused_features)] // so we can still feature(os) when testing
-#![feature(collections, path, io, core, os)]
+#![feature(collections, path, io, core, os, plugin)]
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
+#[plugin] #[no_link] extern crate docopt_macros;
 
 use std::os;
-use docopt::Docopt;
 
 mod elem;
 mod parser;
@@ -12,7 +12,7 @@ mod error;
 mod database;
 mod mass;
 
-const USAGE: &'static str = "
+docopt!(Args, "
 usage:
     chemtool <formula> [options]
     chemtool [-h | --help]
@@ -22,27 +22,18 @@ options:
     -h --help       Display this message and then exit.
     -v --version    Display the version number and then exit.
     --db-path PATH  Explicitly specify the path to the database file.
-";
+");
 
 const VERSION: &'static str = "chemtool 0.2.0";
 
-#[derive(RustcDecodable)]
-struct Args {
-    arg_formula: String,
-    flag_db_path: Option<String>,
-}
-
 #[cfg(not(test))]
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| {
-                                d.help(true)
-                                 .version(Some(VERSION.to_string()))
-                                 .decode()
-                            })
-                            .unwrap_or_else(|e| e.exit());
-    let path = if let Some(path) = args.flag_db_path {
-        Path::new(path)
+    let args: Args = Args::docopt().help(true)
+                             .version(Some(VERSION.to_string()))
+                             .decode()
+                             .unwrap_or_else(|e| e.exit());
+    let path = if !args.flag_db_path.is_empty() {
+        Path::new(args.flag_db_path)
     } else {
         let mut path = Path::new(&os::args()[0]);
         path.set_filename("elemdb.csv");

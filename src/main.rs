27e9285f4a8ed639c5c2b4e2bdf944f3bbbe0dc2
Patrick M Jensen <patmjen@gmail.com>
2015-02-02 19:50:07 +0100

    Replace docopt with getopts
    Since docopt took way too long to run, it has been replaced with
    getopts, which, at least seems to be, faster.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![allow(unused_features)] // so we can still feature(os) when testing
 #![feature(collections, path, io, core, os, plugin)]
-extern crate "rustc-serialize" as rustc_serialize;
-extern crate docopt;
-#[plugin] #[no_link] extern crate docopt_macros;
+extern crate getopts;
 
+use getopts::Options;
 use std::os;
 
 mod elem;
@@ -12,35 +11,49 @@ mod error;
 mod database;
 mod mass;
 
-docopt!(Args, "
-usage:
-    chemtool <formula> [options]
+const USAGE: &'static str = "\
+Usage:
+    chemtool <formula>... [options]
     chemtool [-h | --help]
-    chemtool [-v | --version]
-
-options:
-    -h --help       Display this message and then exit.
-    -v --version    Display the version number and then exit.
-    --db-path PATH  Explicitly specify the path to the database file.
-");
+    chemtool [-v | --version]";
 
 const VERSION: &'static str = "chemtool 0.2.0";
 
 #[cfg(not(test))]
 fn main() {
-    let args: Args = Args::docopt().help(true)
-                             .version(Some(VERSION.to_string()))
-                             .decode()
-                             .unwrap_or_else(|e| e.exit());
-    let path = if !args.flag_db_path.is_empty() {
-        Path::new(args.flag_db_path)
-    } else {
-        let mut path = Path::new(&os::args()[0]);
-        path.set_filename("elemdb.csv");
-        path
+    let args = os::args();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Display this message and then exit.");
+    opts.optflag("v", "version", "Display the version number and then exit.");
+    opts.optopt("", "db-path", "Explicitly specify the path to the database file.", "PATH");
+    let given_opts = match opts.parse(args.tail()) {
+        Ok(go) => go,
+        Err(msg) => {
+            println!("{}", msg.to_string());
+            println!("{}", opts.usage(USAGE));
+            return;
+        },
     };
-    let input = args.arg_formula.as_slice();
-    if let Err(e) = mass::pretty_print_mass(input, &path) {
-        e.print(input);
+    if given_opts.opt_present("help") {
+        println!("{}", opts.usage(USAGE));
+    } else if given_opts.opt_present("version") {
+        println!("{}", VERSION);
+    } else {
+        let path = if let Some(path) = given_opts.opt_str("db-path") {
+            Path::new(path)
+        } else {
+            let mut path = Path::new(&args[0]);
+            path.set_filename("elemdb.csv");
+            path
+        };
+        if given_opts.free.len() > 0 {
+            let input = given_opts.free[0].as_slice();
+            if let Err(e) = mass::pretty_print_mass(input, &path) {
+                e.print(input);
+            }
+        } else {
+            println!("Missing formula to parse.");
+            println!("{}", opts.usage(USAGE));
+        }
     }
 }
